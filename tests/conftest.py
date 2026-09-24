17e980bb46c334b9dd17d8f380faa413924eb680
Aldo <aldo@aldo-f.github.io>
2026-09-24 21:31:36 +0200

    fix: remove root conftest.py to avoid pytest conflicts

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,22 +1,24 @@
-import subprocess, sys, os, time, urllib.request, shutil
+import subprocess, sys, os, time, urllib.request, socket
 import pytest
 
 
 @pytest.fixture(scope="session", autouse=True)
 def start_http_server():
     """Start a simple HTTP server on port 8000 serving the built site."""
-    site_dir = os.path.join(os.path.dirname(__file__), "site")
-    # Start server
+    import subprocess, sys, os, time, urllib.request
+    site_dir = os.path.join(os.path.dirname(__file__), "..", "site")
+    site_dir = os.path.abspath(site_dir)
+    
     proc = subprocess.Popen(
         [
             sys.executable,
             "-m",
             "http.server",
             "8000",
-            "--directory",
-            site_dir,
             "--bind",
             "127.0.0.1",
+            "--directory",
+            site_dir,
         ],
         stdout=subprocess.DEVNULL,
         stderr=subprocess.DEVNULL,
@@ -32,7 +34,9 @@ def start_http_server():
         except Exception:
             time.sleep(0.5)
     else:
-        pytest.fail("HTTP server failed to start")
+        proc.terminate()
+        pytest.fail(f"HTTP server failed to start on port 8000")
+    
     yield
     proc.terminate()
     proc.wait()
